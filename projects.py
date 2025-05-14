d0aa2cf09e649c9c0974ee9ec2694c335a1eb02e
Andy Goldstein <andygo52@gmail.com>
2025-05-14 05:50:36 -0400

    Add files via upload
    Initialize file for Projects for 100 Days of Coding.

---
+++ projects.py
@@ -0,0 +1,19 @@
+#Projects from 100 Days of Coding - Python Pro Bootcamp
+
+#Who Pays the Bill?
+
+import random
+
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+random_number = random.randint(0,4)
+
+print(friends[random_number])
+
+
+
+
+
+
+
+
